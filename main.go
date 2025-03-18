e8186ea0ac4646a7ac2f4032718b7dd6b1d26e02
Charlie Taylor <peekoecode@gmail.com>
2025-03-18 01:23:54 -0700

    logging! and port

--- main.go
+++ main.go
@@ -53,8 +53,19 @@ func main() {
 	fileServer := http.FileServer(http.Dir("./static"))
 	mux.Handle("GET /static/", http.StripPrefix("/static/", fileServer))
 
-	fmt.Println("Server running on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", mux))
+	port := 9000
+	fmt.Println("Server running on http://localhost:", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), logRequest(mux))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s\n", r.RemoteAddr, r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
